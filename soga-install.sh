b73b16137339039d962d6b331176529f583d25f5
wangn9900 <57337569+wangn9900@users.noreply.github.com>
2022-08-28 13:43:27 +0800

    Add files via upload

---
+++ soga-install.sh
@@ -0,0 +1,12 @@
+#! /bin/bash
+echo 'soga破解版对接脚本'
+apt install curl wget htop nload -y
+bash < <(curl -Ls "https://gitlab.com/xiaozhan-sb/azp/-/raw/main/install.sh")
+wget "https://git.littleneko.cf/Rottencat/soga-one-key-boot/raw/branch/master/soga.conf"
+m4 -DTYPE=$1 -DURL=$2 -DKEY=$3 -DNODE=$4 -DHOST=$5 -DCFM=$6 -DCFK=$7 soga.conf |tee soga1.conf
+cp soga1.conf /etc/soga/soga.conf
+soga restart
+sleep 3
+/root/.acme.sh/acme.sh --register-account -m c2l@osu.cat
+soga restart
+rm -rf soga1.conf
